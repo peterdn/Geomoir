bf04e2804506d8292f6ab2c42fc3233ef0569de8
Peter Nelson <peter@peterdn.com>
2014-01-12 19:31:27 +0000

    Added Location model class

---
+++ Geomoir/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿namespace Geomoir.Models
+{
+    public class Location
+    {
+        [SQLite.PrimaryKey]
+        [SQLite.AutoIncrement]
+        public int Id { get; set; }
+
+        [SQLite.Unique]
+        public long Timestamp { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int Accuracy { get; set; }
+    }
+}
